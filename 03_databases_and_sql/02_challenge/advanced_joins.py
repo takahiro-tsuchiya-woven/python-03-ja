0a050b0f188a857eaa0ec3ec2126d56b87e5b19d
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-07-10 18:29:51 +0900

    Update SQL

--- 03_databases_and_sql/02_challenge/advanced_joins.py
+++ 03_databases_and_sql/02_challenge/advanced_joins.py
@@ -9,29 +9,74 @@ db = conn.cursor()
 
 # 楽曲の詳細
 def detailed_tracks(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+        SELECT t.Name, a1.Title, a2.Name 
+        FROM tracks t 
+        LEFT OUTER JOIN albums a1
+        ON t.AlbumId = a1.AlbumId 
+        LEFT OUTER JOIN artists a2
+        ON a1.ArtistId = a2.ArtistId
+    """  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # 未購入の楽曲
 def tracks_not_bought(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+        SELECT t.Name 
+        FROM tracks t
+        LEFT OUTER JOIN invoice_items ii 
+        ON t.TrackId = ii.TrackId 
+        WHERE ii.TrackId is NULL 
+        ORDER by t.Name
+    """  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # ジャンルの統計情報
 def genre_stats(db, genre_name):
-    query = ""  # ここにSQLクエリを書いてください
-    db.execute(query)
-    results = db.fetchall()
-    return results
+    query = """
+        SELECT
+            g.Name,
+            COUNT(t.TrackId) AS number_of_tracks,
+            AVG(t.Milliseconds / 1000.0) AS avg_track_length
+        FROM
+            genres g
+        JOIN
+            tracks t ON g.GenreId = t.GenreId
+        WHERE
+            g.Name = ?
+        GROUP BY
+            g.Name
+    """  # ここにSQLクエリを書いてください
+    db.execute(query, (genre_name,))
+    results = db.fetchone()
+    print(results)
+    if results:
+        return {
+            'genre': results[0],
+            'number_of_tracks': results[1],
+            'avg_track_length': results[2]
+        }
+    else:
+        return None
 
 # ジャンル別のトップ5アーティスト
 def top_five_artists_by_genre(db, genre_name):
-    query = ""  # ここにSQLクエリを書いてください
-    db.execute(query)
+    query = """
+    SELECT artists.Name AS artist_name, COUNT(tracks.TrackId) AS number_of_tracks
+    FROM genres
+    JOIN tracks ON genres.GenreId = tracks.GenreId
+    JOIN albums ON tracks.AlbumId = albums.AlbumId
+    JOIN artists ON albums.ArtistId = artists.ArtistId
+    WHERE genres.Name = ?
+    GROUP BY artists.Name
+    ORDER BY number_of_tracks DESC, artists.Name ASC
+    LIMIT 5
+    """  # ここにSQLクエリを書いてください
+    db.execute(query, (genre_name,))
     results = db.fetchall()
     return results
 
